56a8aaccf538b5324e9337942d88b783ca56e747
PStar <shlin65@me.com>
2017-07-02 20:42:18 +0800

    Create project.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webfetcher
 A small crawler for web data.
+
+## 司法院-裁判書查詢系統
+Main Class: org.pstar.webfetcher.web.judicial.fjud.ui.AppWindow
---
+++ src/org/pstar/webfetcher/web/judicial/fjud/ui/AppWindow.java
@@ -0,0 +1,43 @@
+package org.pstar.webfetcher.web.judicial.fjud.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AppWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppWindow window = new AppWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
